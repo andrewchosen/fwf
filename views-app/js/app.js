8622ecd61ca169ae96250640e0d55d9f6c51349c
andrewchosen <andrewchosen@fullsail.edu>
2015-07-21 18:37:54 -0700

    fixed html names

--- views-app/js/app.js
+++ views-app/js/app.js
@@ -4,10 +4,10 @@ angular.module("CrudApp", ["ngRoute"])
 
 	.config(function($routeProvider){
 		$routeProvider.when("/view1",{
-			templateUrl : "View1.html",
+			templateUrl : "view1.html",
 			controller : "View1Controller"
 		}).when("/view2",{
-			templateUrl : "View2.html",
+			templateUrl : "view2.html",
 			controller : "View2Controller"
 		}).otherwise({
 			redirectTo : "/view1"
