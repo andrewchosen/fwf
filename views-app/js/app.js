a9a0731133cbcd06257e43a5c2d8963683f5bfc4
andrewchosen <andrewchosen@fullsail.edu>
2015-07-21 18:26:55 -0700

    week 3 stuff

---
+++ views-app/js/app.js
@@ -0,0 +1,27 @@
+// Created by Andrew Lancaster on 7/16/2015
+
+angular.module("CrudApp", ["ngRoute"])
+
+	.config(function($routeProvider){
+		$routeProvider.when("/view1",{
+			templateUrl : "View1.html",
+			controller : "View1Controller"
+		}).when("/view2",{
+			templateUrl : "View2.html",
+			controller : "View2Controller"
+		}).otherwise({
+			redirectTo : "/view1"
+		});
+	})
+
+	.controller("View1Controller", function($scope){
+		$scope.test = "Hello World 1";
+	})
+
+	.controller("View2Controller", function($scope){
+		$scope.test = "Hello World 2";
+	})
+
+	.controller("MainController", function($scope){
+		$scope.test = "Hello World";
+	})
